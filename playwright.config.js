d96d724e57091feecb976ce7242511792f14fc92
vijayashree <vijayashree80sv@gmail.com>
2026-03-23 22:30:07 -0500

    playwright configjs update

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
-// @ts-check
+
 import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 import dotenv from 'dotenv';
@@ -13,41 +13,28 @@ const testDir = defineBddConfig({
   ],
 });
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-   dotenv.config({ path: '.env' });
+// Read environment variables from file.
+    dotenv.config({ path: '.env' });
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
+
 export default defineConfig({
   testDir,
-  /* Run tests in files in parallel */
   fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : 2,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
     ['allure-playwright', {
-      outputFolder: 'allure-results',  // MUST match Jenkins results path
+      outputFolder: 'allure-results',  
     }]
   ],
   timeout: 30* 1000,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
      baseURL: process.env.baseUrl,
      headless: true, 
      slowMo: process.env.CI ? 0 : 1000, 
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
      trace: 'on-first-retry',
      screenshot: 'only-on-failure',
      video: 'retain-on-failure',
