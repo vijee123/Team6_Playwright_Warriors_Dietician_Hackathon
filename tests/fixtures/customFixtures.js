bba44d0d18c3350bf92d667aaca4b64b2f1fd9e1
niveditakm <nivedita.km@gmail.com>
2026-03-26 10:17:48 -0400

    Page fixture step changes

--- tests/fixtures/customFixtures.js
+++ tests/fixtures/customFixtures.js
@@ -1,36 +1,54 @@
 import{test as base} from 'playwright-bdd';
-import LoginPage from '../../pages/loginPage.js';
+import LoginPage from '../../pages/LoginPage.js'
+import DashboardPage from '../../pages/DashboardPage.js';
 import EditPatientPage from '../../pages/editPatientPage.js';
-
+import MyPatientsPage from '../../pages/MyPatientsPage.js';
+import DeletePatientPopupPage from '../../pages/deletePatientPopupPage.js';
 
 export const test = base.extend({
-
-    // Add Login fixture 
+    // Login fixture 
     loginPageFixture:async({page},use)=>{
         console.log("Inside the loginPage Fixture");
         const loginPage = new LoginPage(page);
-        await loginPage.navigateToUrl();
+        await loginPage.goto();
         await use(loginPage);
     },
+    // Dashboard fixture
+     dashboardPageFixture:async({page},use)=>{
+        console.log("Using the Dashboard Fixture");
+        const dashboardPage = new DashboardPage(page);
+        await dashboardPage.goto();
+        await use(dashboardPage);
+    },
+    // my patients fixture
+     myPatientPageFixture:async({page},use)=>{
+        console.log("Using the My Patient Fixture");
+        const myPatientPage = new MyPatientsPage(page);
+        await myPatientPage.gotoDashboard();
+        await use(myPatientPage);
+    },
 
-    // Add Dashboard fixture
-    // dashboardPageFixture:async({page},use)=>{
-    //     console.log("Using the Dashboard Fixture");
-    //     const dashboardPage = new DashboardPage(page);
-    //     await use(dashboardPage);
-    // },
-
+  
     // editPatient fixture
     editPatientFixture:async({page},use)=>{
           console.log("Inside the editPatientPage Fixture");
           const editPatientPage = new EditPatientPage(page);
           await editPatientPage.navigateToUrl();
           await use(editPatientPage);       
+    },
+
+    // DeletePatient fixture
+     deletePatientFixture:async({page},use)=>{
+          console.log("Inside the editPatientPage Fixture");
+          const deletePatientPage = new DeletePatientPopupPage(page);
+          await deletePatientPage.navigateToUrl();
+          await use(deletePatientPage);       
     }
-
-    
-
-    
-
-
+   
+    //View Patient Report Fixture
+    viewTestReportFixture:async({page},use)=>{
+        console.log("Inside the viewTestReport Fixture");
+        const viewTestReportPage = new ViewTestReportPage(page);
+        await viewTestReportPage.navigateToUrl();
+        await use(viewTestReportPage);
 });
